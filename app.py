1b0baace5013b19688c9f11b99eefaa24b285a4b
vaibhav19sharma <31787798+vaibhav19sharma@users.noreply.github.com>
2018-06-08 09:45:55 -0400

    mongodb integration

--- app.py
+++ app.py
@@ -2,7 +2,25 @@ from flask import Flask, Response, request, render_template, redirect, url_for, 
 from flaskext.mysql import MySQL
 import datetime, os, shutil
 
+from pymongo import MongoClient
+
 app = Flask(__name__)
+client = MongoClient()
+client = MongoClient('localhost', 27017)
+
+db = client['pymongo_test']
+
+
+posts = db.posts
+post_data = {
+    'title': 'Python and MongoDB',
+    'content': 'PyMongo is fun, you guys',
+    'author': 'Scott'
+}
+result = posts.insert_one(post_data)
+print('One post: {0}'.format(result.inserted_id))
+bills_post = posts.find_one({'title': 'Python and MongoDB'})
+print(bills_post)
 
 
 @app.route('/')
