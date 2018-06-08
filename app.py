66004dad1694b29176296fbb5aed6ac5ab87f1be
vaibhav19sharma <31787798+vaibhav19sharma@users.noreply.github.com>
2018-06-08 00:44:30 -0400

    First Commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, Response, request, render_template, redirect, url_for, session
+from flaskext.mysql import MySQL
+import datetime, os, shutil
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('homescreen.html')
+
+
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    username = request.form['username']
+    password = request.form['pass']
+    if(username =="admin" and password =="admin"):
+        return render_template('index.html', data="Invalid Credentials")
+
+if __name__ == '__main__':
+    app.run()
