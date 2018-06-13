c8a6cffbd7247d42381cd72843e197a3b9b275a3
vaibhav19sharma <31787798+vaibhav19sharma@users.noreply.github.com>
2018-06-13 13:31:29 -0400

    Functionality for adding users to DB and displaying them on homepage

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, request, render_template, redirect, url_for, session
 from flaskext.mysql import MySQL
 import datetime, os, shutil
+from bson.objectid import ObjectId
 
 from pymongo import MongoClient
 
@@ -11,18 +12,10 @@ client = MongoClient('localhost', 27017)
 db = client['pymongo_test']
 
 
-posts = db.posts
-post_data = {
-    'title': 'Python and MongoDB',
-    'content': 'PyMongo is fun, you guys',
-    'author': 'Scott'
-}
-result = posts.insert_one(post_data)
-print('One post: {0}'.format(result.inserted_id))
-bills_post = posts.find_one({'title': 'Python and MongoDB'})
-print(bills_post)
-
-
+posts = db.projectX
+for i in posts.find({}):
+    print(i['_id'])
+temp = posts.find_one({'name': 'Vaibhav Sharma'})
 @app.route('/')
 def hello_world():
     return render_template('homescreen.html')
@@ -33,13 +26,64 @@ def login():
     username = request.form['username']
     password = request.form['pass']
     if(username == "admin" and password == "admin"):
-        return render_template('index.html', data="Invalid Credentials")
+        tempData = []
+        cursor = posts.find({})
+        if cursor is not None:
+            for i in cursor:
+                tempData.append((i['name'],i['_id']))
+        else:
+            tempData = ["Add users"]
+        print(tempData)
+        return render_template('index.html', data=tempData)
 
 
-@app.route('/login/register')
+
+
+@app.route('/login/register', methods = ['GET','POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'GET':
+        return render_template('register.html')
+    if request.method == 'POST':
+        values=["name","dob","ssn","mother","pno","email","add","fwdadd","hardinq","ssc","dl","ub","ps","empat","annsal","lenemp","emppno","empemail"]
+        insVal= dict()
+        for i in values:
+            print(i)
+            if request.form[i] =='':
+                insVal[i] = "No Data Added"
+            else:
+                print(request.form[i])
+                insVal[i] = request.form[i]
+
+        print(insVal)
+        posts.insert(insVal)
+        print("inserted successfully")
+        tempData = []
+        cursor = posts.find({})
+        if cursor is not None:
+            for i in cursor:
+                tempData.append((i['name'],i['_id']))
+        else:
+            tempData = ["Add users"]
+        print(tempData)
+        return render_template('index.html', data=tempData)
 
 
+@app.route('/userInfo/<id>', methods=['POST', 'GET'])
+def userInfo(id):
+    print("-----------------")
+    cursor = posts.find({'_id': ObjectId(id)})
+    temp = dict()
+    for i in cursor:
+        temp = i
+    print(temp)
+    tList = []
+    for k,v in temp.items():
+        if k == '_id':
+            continue
+        else:
+            tList.append((k,v))
+
+    print(tList)
+    return render_template('userInformation.html', info = tList)
 if __name__ == '__main__':
     app.run()
