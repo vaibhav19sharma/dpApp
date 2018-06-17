8c5c97b276177261e8009ba2403443e2f889482f
vaibhav19sharma <31787798+vaibhav19sharma@users.noreply.github.com>
2018-06-16 22:23:23 -0400

    Fixed bugs for request.files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, request, render_template, redirect, url_for, session
 from flaskext.mysql import MySQL
 import datetime, os, shutil
+from bson import Binary
 from bson.objectid import ObjectId
 
 from pymongo import MongoClient
@@ -11,11 +12,16 @@ client = MongoClient('localhost', 27017)
 
 db = client['pymongo_test']
 
-
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 posts = db.projectX
 for i in posts.find({}):
     print(i['_id'])
 temp = posts.find_one({'name': 'Vaibhav Sharma'})
+
+
+
+
 @app.route('/')
 def hello_world():
     return render_template('homescreen.html')
@@ -33,7 +39,7 @@ def login():
                 tempData.append((i['name'],i['_id']))
         else:
             tempData = ["Add users"]
-        print(tempData)
+
         return render_template('index.html', data=tempData)
 
 
@@ -44,19 +50,26 @@ def register():
     if request.method == 'GET':
         return render_template('register.html')
     if request.method == 'POST':
-        values=["name","dob","ssn","mother","pno","email","add","fwdadd","hardinq","ssc","dl","ub","ps","empat","annsal","lenemp","emppno","empemail"]
+        values=["name","dob","ssn","mother","pno","email",'emailPass',"add","fwdadd","hardinq","empat","annsal","lenemp","emppno","empemail"]
         insVal= dict()
         for i in values:
-            print(i)
             if request.form[i] =='':
                 insVal[i] = "No Data Added"
             else:
                 print(request.form[i])
                 insVal[i] = request.form[i]
+        cursor = posts.find({})
+        lstUser = ()
 
-        print(insVal)
+        f = request.files['ssc']
         posts.insert(insVal)
         print("inserted successfully")
+
+
+        for i in cursor:
+            lstUser = (i['_id'])
+        print("Last value is {}".format(str(lstUser)))
+
         tempData = []
         cursor = posts.find({})
         if cursor is not None:
