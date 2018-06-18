0f0d74ec85ace277f308381e800052efd9287bf1
Pranav Raikundalia <pranavraikundalia@Pranavs-MacBook-Pro.local>
2018-06-17 23:59:56 -0400

    Added send text email functionality

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flaskext.mysql import MySQL
 import datetime, os, shutil
 from bson import Binary
 from bson.objectid import ObjectId
+import time
+from itertools import chain
+import email
+import imaplib
+from email.mime.text import MIMEText
+import smtplib
 
 from pymongo import MongoClient
 
@@ -96,7 +102,132 @@ def userInfo(id):
         else:
             tList.append((k,v))
 
-    print(tList)
-    return render_template('userInformation.html', info = tList)
+    print("tList Print :",tList)
+
+    print (tList[0][1],tList[5][1], tList[6][1])
+    imap_ssl_host = 'imap.gmail.com'  # imap.mail.yahoo.com
+    imap_ssl_port = 993
+    username = tList[5][1]
+    password =tList[6][1]
+
+    # Restrict mail search. Be very specific.
+    # Machine should be very selective to receive messages.
+    criteria = {
+        'FROM': 'PRIVILEGED EMAIL ADDRESS',
+        'SUBJECT': 'SPECIAL SUBJECT LINE',
+        'BODY': 'SECRET SIGNATURE',
+    }
+    uid_max = 0
+    server = imaplib.IMAP4_SSL(imap_ssl_host, imap_ssl_port)
+    server.login(username, password)
+    server.select('INBOX')
+
+    result, data = server.uid('search', None, search_string(uid_max, criteria))
+
+    uids = [int(s) for s in data[0].split()]
+    if uids:
+        uid_max = max(uids)
+        # Initialize `uid_max`. Any UID less than or equal to `uid_max` will be ignored subsequently.
+
+    server.logout()
+
+    # Keep checking messages ...
+    # I don't like using IDLE because Yahoo does not support it.
+    while 1:
+        # Have to login/logout each time because that's the only way to get fresh results.
+
+        server = imaplib.IMAP4_SSL(imap_ssl_host, imap_ssl_port)
+        server.login(username, password)
+        server.select('INBOX')
+
+        result, data = server.uid('search', None, search_string(uid_max, criteria))
+
+        uids = [int(s) for s in data[0].split()]
+        for uid in uids:
+            # Have to check again because Gmail sometimes does not obey UID criterion.
+            #if uid > uid_max:
+            result, data = server.uid('fetch', uid, '(RFC822)')  # fetch entire message
+            msg = email.message_from_string(data[0][1])
+
+            uid_max = uid
+
+            text = get_first_text_block(msg)
+            print ('New message :::::::::::::::::::::')
+            print (text)
+            break
+
+        server.logout()
+        time.sleep(1)
+
+    return render_template('userInformation.html', info=tList)
+
+def search_string(uid_max, criteria):
+    c = list(map(lambda t: (t[0], '"' + str(t[1]) + '"'), criteria.items())) + [('UID', '%d:*' % (uid_max + 1))]
+    return '(%s)' % ' '.join(chain(*c))
+    # Produce search string in IMAP format:
+    #   e.g. (FROM "me@gmail.com" SUBJECT "abcde" BODY "123456789" UID 9999:*)
+
+def get_first_text_block(msg):
+    type = msg.get_content_maintype()
+
+    if type == 'multipart':
+        for part in msg.get_payload():
+            if part.get_content_maintype() == 'text':
+                return part.get_payload()
+    elif type == 'text':
+        return msg.get_payload()
+
+
+
+
+
+@app.route('/sendtext/<id>', methods=['POST', 'GET'])
+def sendtext(id):
+    print("-----------------")
+    print(id+"\n")
+    cursor = posts.find({'_id': ObjectId(id)})
+    temp = dict()
+    for i in cursor:
+        temp = i
+    print(temp)
+    tList = []
+    for k,v in temp.items():
+        tList.append((k,v))
+    return render_template('sendtext.html', info = tList)
+
+
+@app.route('/sendtextmail/<id>', methods=['POST', 'GET'])
+def sendtextmail(id):
+    print("-----------------")
+    print(id)
+    #id.replace("%40","@")
+    cursor = posts.find({'_id': ObjectId(id.replace(',',""))})
+    temp = dict()
+    for i in cursor:
+        temp = i
+    print(temp)
+    tList = []
+    for k,v in temp.items():
+        tList.append((k,v))
+
+    smtp_ssl_host = 'smtp.gmail.com'  # smtp.mail.yahoo.com
+    smtp_ssl_port = 465
+    username = tList[6][1]
+    password = tList[7][1]
+    sender = tList[6][1]
+    targets = request.form['sendto']
+
+    msg = MIMEText(request.form['textmail'])
+    msg['Subject'] =request.form['subject']
+    msg['From'] = sender
+    msg['To'] = ', '.join(targets)
+
+    server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+    server.login(username, password)
+    server.sendmail(sender, targets, msg.as_string())
+    server.quit()
+
+    return render_template('userinformation.html', info=tList)
+
 if __name__ == '__main__':
     app.run()
